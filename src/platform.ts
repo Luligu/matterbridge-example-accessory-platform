2034c05f8471348d8ca6ff6c287b3655eae31deb
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-12 17:30:04 +0100

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -4,6 +4,8 @@ import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform } from 
 import { AnsiLogger } from 'node-ansi-logger';
 
 export class ExampleMatterbridgeAccessoryPlatform extends MatterbridgeAccessoryPlatform {
+  cover: MatterbridgeDevice | undefined;
+
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
   }
@@ -11,23 +13,28 @@ export class ExampleMatterbridgeAccessoryPlatform extends MatterbridgeAccessoryP
   override async onStart(reason?: string) {
     this.log.info('onStart called with reason:', reason ?? 'none');
 
-    const cover = new MatterbridgeDevice(DeviceTypes.WINDOW_COVERING);
-    cover.createDefaultIdentifyClusterServer();
-    cover.createDefaultBasicInformationClusterServer('Accessory device', '0x59108853', 0xfff1, 'Luligu', 0x0001, 'Accessory device');
-    cover.createDefaultPowerSourceWiredClusterServer();
-    cover.createDefaultWindowCoveringClusterServer(10000);
-    await this.registerDevice(cover);
+    this.cover = new MatterbridgeDevice(DeviceTypes.WINDOW_COVERING);
+    this.cover.createDefaultIdentifyClusterServer();
+    this.cover.createDefaultBasicInformationClusterServer('Accessory device', '0x59108853', 0xfff1, 'Luligu', 0x0001, 'Accessory device');
+    this.cover.createDefaultPowerSourceWiredClusterServer();
+    this.cover.createDefaultWindowCoveringClusterServer(10000);
+    await this.registerDevice(this.cover);
 
-    cover.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
+    this.cover.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
       this.log.info(`Command identify called identifyTime:${identifyTime}`);
     });
 
-    cover.addCommandHandler('goToLiftPercentage', async ({ request: { liftPercent100thsValue } }) => {
+    this.cover.addCommandHandler('goToLiftPercentage', async ({ request: { liftPercent100thsValue } }) => {
       this.log.info(`Command goToLiftPercentage called liftPercent100thsValue:${liftPercent100thsValue}`);
     });
+  }
+
+  override async onConfigure() {
+    this.log.info('onConfigure called');
 
     setInterval(() => {
-      const coverCluster = cover.getClusterServer(WindowCoveringCluster.with(WindowCovering.Feature.Lift, WindowCovering.Feature.PositionAwareLift));
+      if (!this.cover) return;
+      const coverCluster = this.cover.getClusterServer(WindowCoveringCluster.with(WindowCovering.Feature.Lift, WindowCovering.Feature.PositionAwareLift));
       if (coverCluster && coverCluster.getCurrentPositionLiftPercent100thsAttribute) {
         let position = coverCluster.getCurrentPositionLiftPercent100thsAttribute()! + 1000;
         position = position > 10000 ? 0 : position;
